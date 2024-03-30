1b4140ae40d9e2499e29dfc2e29337a584618e82
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-03-29 23:16:50 -0500

    fixed account page rendering

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,50 +1,12 @@
 import * as React from 'react';
-import API from "@/tools/API";
-import Button from "@mui/material/Button";
-//import {useState} from "react";
+import AccountList from "@/components/account/AccountList";
 
 export default function Account() {
 
-    // TODO: Working on accounts list, since that requires no event handler... apparently I can't use useState in a server component?
-    // This is how I'm used to it with react, and googling was telling me the same thing for nextjs. I am at a loss
-    // const [accounts, setAccounts] = useState([]);
-    // const [loading, setLoading] = useState(true);
-
-    async function handleCreate(){
-        try{
-            const {status, isOk, message} = await API.createAccount("Test Account", 333);
-            if(isOk){
-                // TODO add toasts
-                console.log("Success: message:", message);
-            }else{
-                // TODO add toasts
-                console.error("Error: message:", message);
-            }
-        }catch (e) {
-            // TODO add toasts
-            console.error("Error:", e);
-        }
-    }
-
-    const fetchAccounts = async () => {
-        try{
-            const resp = await API.getAccounts("");
-            if(resp.isOk){
-                //TODO: setAccounts(resp.data); webstorm is telling me ResponseResult has no data field?
-            }else{
-                console.error("Error: message:", resp.message);
-            }
-        }catch (e) {
-            console.error("Error:", e);
-        }
-        // setLoading(false);
-
-    };
 
     return (
         <div>
-            <h1>Account</h1>
-            <Button onClick={handleCreate}>Create Account</Button>
+            <AccountList/>
         </div>
     );
 }
---
+++ src/components/account/AccountList.tsx
@@ -0,0 +1,64 @@
+"use client"
+import * as React from 'react';
+import {useState} from "react";
+import API from "@/tools/API";
+import Button from '@mui/material/Button';
+
+export default function AccountList() {
+    const [accounts, setAccounts] = useState<any[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    const fetchAccounts = async () => {
+        try {
+            const resp = await API.getAccounts("");
+            if (resp.isOk) {
+                if ("data" in resp) {
+                    setAccounts(resp.data);
+                }
+            } else {
+                console.error("Error: message:", resp.message);
+            }
+        } catch (e) {
+            console.error("Error:", e);
+        }
+        setLoading(false);
+
+    };
+
+    async function handleCreate() {
+        try {
+            const {status, isOk, message} = await API.createAccount("Test Account", 333);
+            if (isOk) {
+                // TODO add toasts
+                console.log("Success: message:", message);
+            } else {
+                // TODO add toasts
+                console.error("Error: message:", message);
+            }
+        } catch (e) {
+            // TODO add toasts
+            console.error("Error:", e);
+        }
+    }
+
+    React.useEffect(() => {
+        fetchAccounts().then(r => console.log("Accounts fetched"));
+    }, []);
+
+    return (
+        <div>
+            <h1>Account List</h1>
+            {loading ? (
+                <p>Loading...</p>
+            ) : accounts.length > 0 ? (
+                <ul>
+                    {accounts.map((account, index) => (
+                        <li key={index}>{account.account_name}</li>
+                    ))}
+                </ul>
+            ) : (
+                <><p>No accounts found. Let's fix that!</p><Button variant="contained" onClick={handleCreate}>Create Account</Button></>
+            )}
+        </div>
+    );
+}
