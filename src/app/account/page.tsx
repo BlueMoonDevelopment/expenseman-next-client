2f8470ff5791ea70333dec8a0f32d1d3c3409bd5
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-03-29 19:36:00 -0500

    Account creation (untested)

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,5 +1,28 @@
 import * as React from 'react';
+import API from "@/tools/API";
+import Button from "@mui/material/Button";
 
 export default function Account() {
+    async function handleCreate(){
+        try{
+            const {status, isOk, message} = await API.createAccount("Test Account", 333);
+            if(isOk){
+                // TODO add toasts
+                console.log("Success: message:", message);
+            }else{
+                // TODO add toasts
+                console.error("Error: message:", message);
+            }
+        }catch (e) {
+            // TODO add toasts
+            console.error("Error:", e);
+        }
+    }
 
+    return (
+        <div>
+            <h1>Account</h1>
+            <Button onClick={handleCreate}>Create Account</Button>
+        </div>
+    );
 }
--- src/tools/API.ts
+++ src/tools/API.ts
@@ -6,10 +6,17 @@ const CREATED = 200;
 const BAD_REQUEST = 400;
 const UNAUTHORIZED = 401;
 const FORBIDDEN = 403;
+const INTERNAL_ERROR = 500;
 
-interface StatusResult{
+interface StatusResult {
     statusString: string;
     isOk: boolean;
+}
+
+interface ResponseResult {
+    status: number;
+    isOk: boolean;
+    message: string;
 }
 
 export default class API {
@@ -20,7 +27,7 @@ export default class API {
      * @param status The request response status
      * @return StatusResult - contains the string representation and true if successful, false otherwise
      */
-    static checkStatus(status: number) : StatusResult {
+    static checkStatus(status: number): StatusResult {
         switch (status) {
             case OK:
                 return {statusString: "OK", isOk: true};
@@ -34,6 +41,49 @@ export default class API {
                 console.warn("Unknown status code from request:", status);
                 return {statusString: "UNKNOWN", isOk: false};
         }
+    }
+
+    // TODO add currency and description
+    static async createAccount(name: string, balance: number) {
+        const data = {
+            account_name: name,
+            account_balance: balance
+        };
+        const url = API_ENDPOINT_URL + "/accounts";
+        return await fetch(url, {
+            method: "POST",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        }).then(res => {
+            let resp = {} as ResponseResult;
+            resp.status = res.status;
+            resp.isOk = res.status === 200;
+            switch (res.status) {
+                case OK:
+                    resp.message = "Account created successfully";
+                    break;
+                case BAD_REQUEST:
+                    resp.message = "No account name was provided";
+                    break;
+                case UNAUTHORIZED:
+                    resp.message = "You are unauthorized to do this. Please sign in";
+                    break;
+                case 409:
+                    resp.message = "Account limit reached";
+                    break;
+                default:
+                    resp.message = "Unknown error occurred";
+                    break;
+            }
+
+            return resp;
+        }).catch(err => {
+            console.error("Error creating account:", err);
+            return {status: INTERNAL_ERROR, isOk: false, message: "Internal server error"};
+        });
     }
 
 }
