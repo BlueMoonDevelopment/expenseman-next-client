cafeadf7063670b2c4beb02221da9979a5a165a5
TigerCipher <67520209+TigerCipher@users.noreply.github.com>
2024-04-02 17:07:10 -0500

    Update configuration.example.tsx

--- src/configuration/configuration.example.tsx
+++ src/configuration/configuration.example.tsx
@@ -1,4 +1,4 @@
 const mode = process.env.NODE_ENV || "production";
 export const DEVELOPMENT_MODE = mode === "development";
-export const API_ENDPOINT_URL = DEVELOPMENT_MODE ? "http://localhost:8082" : "https://api.expenseman.app";
+export const API_ENDPOINT_URL = DEVELOPMENT_MODE ? "http://localhost:8082/api" : "https://api.expenseman.app";
 export const GOOGLE_OAUTH_CLIENT_ID: string = "clientid";
