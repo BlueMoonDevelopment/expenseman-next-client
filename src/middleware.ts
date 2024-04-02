2fcd11d985f233eb037f14b91ee6bf8205df760b
TheJeterLP <joey.peter1998@gmail.com>
2024-04-02 17:05:34 +0200

    Test changes to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,31 @@
 import {NextRequest, NextResponse} from "next/server";
-import {API_ENDPOINT_URL} from "@/configuration/configuration";
+import {API_ENDPOINT_URL, DEVELOPMENT_MODE} from "@/configuration/configuration";
 
-// This file is being used by Next.JS to ensure that the app path is authenitcated, if not the user gets redirected to /auth/signin
+// This file is being used by Next.JS to ensure that the app path is authenticated, if not the user gets redirected to /auth/signin
 
 export const middleware = async (request: NextRequest) => {
     const url = API_ENDPOINT_URL + '/auth/checksignedin';
-    const res = await fetch(url, {
-        method: 'GET',
-        credentials: 'include',
-        headers: {Cookie: request.cookies.toString()},
-    }).then(async (res) => await res.status);
 
-    const signed = res == 200;
+    let signed;
 
+    if (DEVELOPMENT_MODE) {
+        const res = await fetch(url, {
+            method: 'GET',
+            credentials: 'include',
+            headers: {Cookie: request.cookies.toString()},
+        }).then(async (res) => await res.status);
+
+        signed = res == 200;
+    } else {
+        const res = await fetch(url, {
+            method: 'GET',
+            credentials: 'include',
+        }).then(async (res) => await res.status);
+    }
     if (!signed) {
         return NextResponse.redirect(new URL('/auth/signin', request.url));
     }
+
 }
 
 export const config = {
