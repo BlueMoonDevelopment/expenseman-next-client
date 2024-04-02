673918dd51c0868813fd6c52fc37743d2a20ff7b
TheJeterLP <joey.peter1998@gmail.com>
2024-04-02 17:26:57 +0200

    Add origin to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,27 +1,23 @@
 import {NextRequest, NextResponse} from "next/server";
 import {API_ENDPOINT_URL, DEVELOPMENT_MODE} from "@/configuration/configuration";
+import * as url from "url";
 
 // This file is being used by Next.JS to ensure that the app path is authenticated, if not the user gets redirected to /auth/signin
 
 export const middleware = async (request: NextRequest) => {
     const url = API_ENDPOINT_URL + '/auth/checksignedin';
 
-    let signed;
+    const res = await fetch(url, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            Cookie: request.cookies.toString(),
+            Origin: API_ENDPOINT_URL,
+        },
+    }).then(async (res) => await res.status);
 
-    if (DEVELOPMENT_MODE) {
-        const res = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-            headers: {Cookie: request.cookies.toString()},
-        }).then(async (res) => await res.status);
+    const signed = res == 200;
 
-        signed = res == 200;
-    } else {
-        const res = await fetch(url, {
-            method: 'GET',
-            credentials: 'include',
-        }).then(async (res) => await res.status);
-    }
     if (!signed) {
         return NextResponse.redirect(new URL('/auth/signin', request.url));
     }
